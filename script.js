b4e603fb6e526220d6a5eb92f288390eae92c064
Nathanael Ogden <104324138+PridefulBlindford@users.noreply.github.com.>
2026-06-19 08:49:40 -0400

    added the variables and play game function
    aded variables for the socre of the human and computer and added a function to allow to play a round.

--- script.js
+++ script.js
@@ -21,6 +21,46 @@ function getHumanChoice(){
 
 }
 
+let humanScore=0;
+let computerScore=0;
 
+function playGame(humanChoice,computerChoice){
+    if(humanChoice === "rock" && computerChoice === "scissors"){
+        console.log("You win! Rock beats scissors.");
+        humanScore++;
+        return;
+    }
+    else if(humanScore === "paper" && computerChoice === "rock"){
+        console.log("You win! Paper beats rock.");
+        humanChoice++;
+        return;
+    }
+    else if(humanChoice === "scissors" && computerChoice === "paper"){
+        console.log("You win! Scissors beats paper.");
+        humanChoice++;
+        return;
+    }
+    else if(computerChoice === "rock" && humanChoice === "scissors"){
+        console.log("You lose! Rock beats scissors.");
+        computerScore++;
+        return;
+    }
+    
+    else if(computerScore === "paper" && humanChoice === "rock"){
+        console.log("you lose! Paper beats rock.");
+        computerScore++;
+        return;
+    }
+    else if(computerScore === "scissors" && humanChoice === "paper"){
+    
+    
+        console.log("You lose! Scissors beats paper.");
+        computerScore++;
+        return;
 
-
+    }
+    else{
+        console.log("It's a tie!");
+        return;
+    }
+}
